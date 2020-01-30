f50085e70721c7f6d5107e9965f2d84473ccbdb3
Aleksei Toropov <45897514+thetoropov@users.noreply.github.com>
2020-01-30 23:43:05 +0300

    add tic-tac-toe-with-ai on 5 stage

--- tic-tac-toe-with-ai.py
+++ tic-tac-toe-with-ai.py
@@ -10,16 +10,37 @@ class EasyStrategy(Strategy):
         print('Making move level "easy"')
         x = random.randint(1, 3)
         y = random.randint(1, 3)
-        coord_ai = [x, y]
-        return coord_ai
+        return [x, y]
 
 
 class MediumStrategy(Strategy):
+    def reformat(self, best):
+        if best[0] == 0 and best[1] == 0:
+            coord_ai = [1, 3]
+        elif best[0] == 0 and best[1] == 1:
+            coord_ai = [2, 3]
+        elif best[0] == 0 and best[1] == 2:
+            coord_ai = [3, 3]
+        elif best[0] == 1 and best[1] == 0:
+            coord_ai = [1, 2]
+        elif best[0] == 1 and best[1] == 1:
+            coord_ai = [2, 2]
+        elif best[0] == 1 and best[1] == 2:
+            coord_ai = [3, 2]
+        elif best[0] == 2 and best[1] == 0:
+            coord_ai = [1, 1]
+        elif best[0] == 2 and best[1] == 1:
+            coord_ai = [2, 1]
+        elif best[0] == 2 and best[1] == 2:
+            coord_ai = [3, 1]
+        return coord_ai
+
     def move(self, fld, symbol):
         x = 0
         y = 0
-        count = 0
-        for i in range(3):
+        print('Making move level "medium"')
+        """
+       for i in range(3):
             if fld[i][0] == fld[i][1] == symbol and fld[i][2] == " ":
                 x = 3
                 y = abs(i - 3)
@@ -101,41 +122,258 @@ class MediumStrategy(Strategy):
             count += 1
         if count == 0:
             for i in range(3):
-                if fld[i][0] != symbol and fld[i][1] != symbol and fld[i][2] == " ":
+                if fld[i][0] != symbol and fld[i][1] != symbol and fld[i][0] != " " and fld[i][1] != " " and fld[i][2] == " ":
                     x = 3
                     y = abs(i - 3)
                     break
-                if fld[i][1] != symbol and fld[i][2] != symbol and fld[i][0] == " ":
+                if fld[i][1] != symbol and fld[i][2] != symbol and fld[i][1] != " " and fld[i][2] != " " and fld[i][0] == " ":
                     x = 1
                     y = abs(i - 3)
                     break
-                if fld[i][0] != symbol and fld[i][2] != symbol and fld[i][1] == " ":
+                if fld[i][0] != symbol and fld[i][2] != symbol and fld[i][0] != " " and fld[i][2] != " " and fld[i][1] == " ":
                     x = 2
                     y = abs(i - 3)
                     break
-                if fld[0][i] != symbol and fld[1][i] != symbol and fld[2][i] == " ":
+                if fld[0][i] != symbol and fld[1][i] != symbol and fld[0][i] != " " and fld[1][i] != " " and fld[2][i] == " ":
                     x = abs(-1 - i)
                     y = 1
                     break
-                if fld[1][i] != symbol and fld[2][i] != symbol and fld[0][i] == " ":
+                if fld[1][i] != symbol and fld[2][i] != symbol and fld[1][i] != " " and fld[2][i] != " " and fld[0][i] == " ":
                     x = abs(-1 - i)
                     y = 3
                     break
-                if fld[0][i] != symbol and fld[2][i] != symbol and fld[1][i] == " ":
+                if fld[0][i] != symbol and fld[2][i] != symbol and fld[0][i] != " " and fld[2][i] != " " and fld[1][i] == " ":
                     x = abs(-1 - i)
                     y = 2
                     break
-        print('Making move level "medium"')
-        if x == 0 and y == 0:
-            x = random.randint(1, 3)
-            y = random.randint(1, 3)
+                    """
+        for i in range(3):
+            count1 = count2 = 0
+            bestJ = 0
+            for j in range(3):
+                if fld[i][j] == symbol:
+                    count1 += 1
+                if fld[i][j] == ' ':
+                    count2 += 1;
+                    bestJ = j
+            if count2 == 1 and count1 == 2:
+                x = i
+                y = bestJ
+                coord_ai = [x, y]
+                return self.reformat(coord_ai)
+            if count2 == 1 and count1 == 0:
+                x = i
+                y = bestJ
+                coord_ai = [x, y]
+                return self.reformat(coord_ai)
+
+        for i in range(3):
+            count1 = count2 = 0
+            for j in range(3):
+                if fld[j][i] == symbol:
+                    count1 += 1
+                if fld[j][i] == ' ':
+                    count2 += 1
+                    bestJ = j
+            if count2 == 1 and count1 == 2:
+                x = bestJ
+                y = i
+                coord_ai = [x, y]
+                return self.reformat(coord_ai)
+            if count2 == 1 and count1 == 0:
+                x = bestJ
+                y = i
+                coord_ai = [x, y]
+                return self.reformat(coord_ai)
+
+        count1 = count2 = 0
+        if fld[0][0] == symbol:
+            count1 += 1
+        if fld[0][0] == ' ':
+            count2 += 1
+            bestI = 0
+            bestJ = 0
+        if fld[1][1] == symbol:
+            count1 += 1
+        if fld[1][1] == ' ':
+            count2 += 1
+            bestI = 1
+            bestJ = 1
+        if fld[2][2] == symbol:
+            count1 += 1
+        if fld[2][2] == ' ':
+            count2 += 1
+            bestI = 2
+            bestJ = 2
+        if count2 == 1 and count1 == 2:
+            x = bestI
+            y = bestJ
+            coord_ai = [x, y]
+            return self.reformat(coord_ai)
+        if count2 == 1 and count1 == 0:
+            x = bestI
+            y = bestJ
+            coord_ai = [x, y]
+            return self.reformat(coord_ai)
+        count1 = count2 = 0
+        if fld[0][2] == symbol:
+            count1 += 1
+        if fld[0][2] == ' ':
+            count2 += 1
+            bestI = 0
+            bestJ = 2
+        if fld[1][1] == ' ':
+            count2 += 1
+            bestI = 1
+            bestJ = 1
+        if fld[1][1] == symbol:
+            count1 += 1
+        if fld[2][0] == symbol:
+            count1 += 1
+        if fld[2][0] == ' ':
+            count2 += 1
+            bestI = 2
+            bestJ = 0
+        if count2 == 1 and count1 == 2:
+            x = bestI
+            y = bestJ
+            coord_ai = [x, y]
+            return self.reformat(coord_ai)
+        if count2 == 1 and count1 == 0:
+            x = bestI
+            y = bestJ
+            coord_ai = [x, y]
+            return self.reformat(coord_ai)
+
+        x = random.randint(0, 2)
+        y = random.randint(0, 2)
         coord_ai = [x, y]
-        return coord_ai
+        return self.reformat(coord_ai)
+
+
+    """ def check_win(self, count_empties, count_my_symbols):
+        if count_empties != 1:
+            return False
+        elif count_my_symbols == 2:
+            return True
+        else:
+            return False
+
+    def is_row_win(self, fld, symbol, row):
+        count_empties = 0
+        count_my_symbols = 0
+        for i in range(3):
+            if fld[row][i] == " ":
+                count_empties += 1
+            if fld[row][i] == symbol:
+                count_my_symbols += 1
+        return self.check_win(count_empties, count_my_symbols)
+
+
+    def is_diag_win(self, fld, symbol, diag):
+        count_empties = 0
+        count_my_symbols = 0
+        for i in range(3):
+            pass
+
+
+    def is_column_win(self, fld, symbol, column):
+        count_empties = 0
+        count_my_symbols = 0
+        for i in range(3):
+            if fld[i][column] == " ":
+                count_empties += 1
+            if fld[i][column] == symbol:
+                count_my_symbols += 1
+        return self.check_win(count_empties,count_my_symbols)
+    """
 
 
 class HardStrategy(Strategy):
+
     def move(self, fld, symbol):
-        pass
+        print('Making move level "hard"')
+        bestScore = -1
+        bestI = 0
+        bestJ = 0
+        for i in range(3):
+            for j in range(3):
+                if fld[i][j] == " ":
+                    score = self.minimax(fld, i, j, 0, symbol)
+                    if bestScore == -1 or (bestScore < score):
+                        bestScore = score
+                        bestI = i
+                        bestJ = j
+        best = [bestI, bestJ]
+        return self.reformat(best)
+
+    def reformat(self, best):
+        if best[0] == 0 and best[1] == 0:
+            coord_ai = [1, 3]
+        elif best[0] == 0 and best[1] == 1:
+            coord_ai = [2, 3]
+        elif best[0] == 0 and best[1] == 2:
+            coord_ai = [3, 3]
+        elif best[0] == 1 and best[1] == 0:
+            coord_ai = [1, 2]
+        elif best[0] == 1 and best[1] == 1:
+            coord_ai = [2, 2]
+        elif best[0] == 1 and best[1] == 2:
+            coord_ai = [3, 2]
+        elif best[0] == 2 and best[1] == 0:
+            coord_ai = [1, 1]
+        elif best[0] == 2 and best[1] == 1:
+            coord_ai = [2, 1]
+        elif best[0] == 2 and best[1] == 2:
+            coord_ai = [3, 1]
+        return coord_ai
+
+    def who_wins(self, fld):
+        for i in range(3):
+            if fld[i][0] == fld[i][1] == fld[i][2] != " ":
+                return "wins"
+        for i in range(3):
+            if fld[0][i] == fld[1][i] == fld[2][i] != " ":
+                return "wins"
+        if fld[0][0] == fld[1][1] == fld[2][2] != " " or fld[0][2] == fld[1][1] == fld[2][0] != " ":
+            return "wins"
+        for i in range(3):
+            for j in range(3):
+                if fld[i][j] == " ":
+                    return "game not finished"
+        return "draw"
+
+    def minimax(self, fld, curI, curJ, turn, symbol):
+        board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        for i in range(3):
+            for j in range(3):
+                board[i][j] = fld[i][j]
+        if turn == 0:
+            board[curI][curJ] = symbol
+        else:
+            if symbol == "O":
+                board[curI][curJ] = "X"
+            else:
+                board[curI][curJ] = "O"
+        bestScore = -1
+
+        if self.who_wins(board) == "wins":
+            if turn == 0:
+                return 10
+            else:
+                return -10
+        elif self.who_wins(board) == "draw":
+            return 0
+        else:
+            pass
+
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == " ":
+                    score = self.minimax(board, i, j, abs(turn - 1), symbol)
+                    if bestScore == -1 or (turn == 1 and bestScore < score) or (turn == 0 and bestScore > score):
+                        bestScore = score
+        return bestScore
 
 
 class Player(object):
@@ -194,61 +432,6 @@ def print_field(fld):
     print("---------")
 
 
-def is_row_fill(fld, row_number, symbol):
-    row = [" ", " ", " "]
-    for i in range(3):
-        row[i] = fld[i][row_number]
-    return count(row, symbol) == 3
-
-
-def is_col_fill(fld, col_number, symbol):
-    col = [" ", " ", " "]
-    for i in range(3):
-        col[i] = fld[col_number][i]
-    return count(col, symbol) == 3
-
-
-def is_diag1_fill(fld, symbol):
-    diag = [fld[0][0], fld[1][1], fld[2][2]]
-    return count(diag, symbol) == 3
-
-
-def is_diag2_fill(fld, symbol):
-    diag = [fld[2][0], fld[1][1], fld[0][2]]
-    return count(diag, symbol) == 3
-
-
-def count(lst, symbol):
-    counter = 0
-    for i in range(len(lst)):
-        if lst[i] == symbol:
-            counter += 1
-    return counter
-
-
-def detect_game(fld):
-    for i in range(3):
-        if is_row_fill(fld, i, "X"):
-            return "X wins"
-        if is_row_fill(fld, i, "O"):
-            return "O wins"
-        if is_col_fill(fld, i, "X"):
-            return "X wins"
-        if is_col_fill(fld, i, "O"):
-            return "O wins"
-    if is_diag1_fill(fld, "X") or is_diag2_fill(fld, "X"):
-        return "X wins"
-    if is_diag1_fill(fld, "O") or is_diag2_fill(fld, "O"):
-        return "O wins"
-    empty_squares = 0
-    for i in range(3):
-        empty_squares += count(fld[i], " ")
-    if empty_squares == 0:
-        return "Draw"
-    else:
-        return "Game not finished"
-
-
 def create_player(type, symbol):
     type = type.lower()
     if type == "easy" or type == "medium" or type == "hard":
@@ -270,12 +453,34 @@ def fill(fld, x, y, symbol):
     field[abs(y - 3)][abs(x - 1)] = symbol
 
 
-def turn():
-    pass
+def is_win(fld, symbol):
+    for i in range(3):
+        if fld[i][0] == fld[i][1] == fld[i][2] == symbol:
+            if symbol == "X":
+                return "X wins"
+            else:
+                return "O wins"
+    for i in range(3):
+        if fld[0][i] == fld[1][i] == fld[2][i] == symbol:
+            if symbol == "X":
+                return "X wins"
+            else:
+                return "O wins"
+    if fld[0][0] == fld[1][1] == fld[2][2] == symbol or fld[0][2] == fld[1][1] == fld[2][0] == symbol:
+        if symbol == "X":
+            return "X wins"
+        else:
+            return "O wins"
+    for i in range(3):
+        for j in range(3):
+            if fld[i][j] == " ":
+                return "Game not finished"
+    return "Draw"
 
 
 field = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-who = True
+
+first_turn_flag = True
 
 while True:
     try:
@@ -291,7 +496,7 @@ while True:
     if command == "start" and len(lst) == 3:
         print_field(field)
         while True:
-            if (who == True):
+            if first_turn_flag:
                 player1 = create_player(type1, "X")
                 coord_player1 = player1.get_move(type1, field)
                 a = coord_player1[0]
@@ -299,9 +504,11 @@ while True:
                 if check_params(a, b) == 0:
                     fill(field, a, b, player1.get_symbol())
                     print_field(field)
-                    who = False
-                    if detect_game(field) != "Game not finished":
-                        print(detect_game(field))
+                    first_turn_flag = False
+                    if is_win(field, player1.get_symbol()) != "Game not finished":
+                        print(is_win(field, player1.get_symbol()))
+                        field = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+                        first_turn_flag = True
                         break
             else:
                 player2 = create_player(type2, "O")
@@ -311,10 +518,11 @@ while True:
                 if check_params(a1, b1) == 0:
                     fill(field, a1, b1, player2.get_symbol())
                     print_field(field)
-                    who = True
-                    if detect_game(field) != "Game not finished":
-                        print(detect_game(field))
+                    first_turn_flag = True
+                    if is_win(field, player2.get_symbol()) != "Game not finished":
+                        print(is_win(field, player2.get_symbol()))
                         field = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+                        first_turn_flag = True
                         break
     elif command == "exit":
         break
